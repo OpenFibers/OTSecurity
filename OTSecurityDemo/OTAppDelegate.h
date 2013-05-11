b498da4a80040df639ea9eae494d93e46901f43d
openthread <openfibers@gmail.com>
2013-05-11 19:41:33 +0800

    Initial Commit

---
+++ OTSecurityDemo/OTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OTAppDelegate.h
+//  OTSecurityDemo
+//
+//  Created by openthread on 5/11/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OTSecurityDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OTSecurityDemo
+//
+//  Created by openthread on 5/11/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OTAppDelegate class]));
+    }
+}
